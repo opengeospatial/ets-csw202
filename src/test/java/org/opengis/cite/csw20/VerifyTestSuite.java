bce949d3fc1a9366379d97a7123b5d4bb551847c
R. Martell <rjmartell@computer.org>
2014-05-05 16:30:07 -0700

    Read test suite parameters from properties file.

--- src/test/java/org/opengis/cite/csw20/VerifyTestSuite.java
+++ src/test/java/org/opengis/cite/csw20/VerifyTestSuite.java
@@ -3,6 +3,7 @@ package org.opengis.cite.csw20;
 import static org.junit.Assert.assertEquals;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.URL;
@@ -33,13 +34,17 @@ import com.occamlab.te.index.Index;
 import com.occamlab.te.util.DocumentationHelper;
 
 /**
- * Verifies the results of executing a CTL test suite.
+ * Verifies the results of executing a CTL test suite. The test suite parameters
+ * are read from a properties file located at ${user.home}/sut.properties; if
+ * this file does not exist then load the default sut.properties file from the
+ * classpath (src/test/resources).
  */
 public class VerifyTestSuite {
 
     static final int VERDICT_PASS = 1;
     static final int VERDICT_FAIL = 6;
     private static Properties testProps;
+    private static Properties sutProps;
     private static String previousTEBase;
     private static DocumentBuilder docBuilder;
     private static final String CSW202_NS = "http://www.opengis.net/cat/csw/2.0.2";
@@ -66,6 +71,21 @@ public class VerifyTestSuite {
         }
         previousTEBase = System.getProperty("TE_BASE");
         System.setProperty("TE_BASE", teBaseDir.getAbsolutePath());
+    }
+
+    @BeforeClass
+    public static void initTestSuiteParams() throws IOException {
+        sutProps = new Properties();
+        String homeDir = System.getProperty("user.home");
+        File sutPropsFile = new File(homeDir, "sut.properties");
+        if (sutPropsFile.exists()) {
+            sutProps.load(new FileInputStream(sutPropsFile));
+        } else {
+            try (InputStream propsInput = VerifyTestSuite.class
+                    .getResourceAsStream("/sut.properties")) {
+                sutProps.load(propsInput);
+            }
+        }
     }
 
     @AfterClass
@@ -98,15 +118,19 @@ public class VerifyTestSuite {
 
     @Test
     public void executeStartingTest() throws Exception {
-        String capabilitiesUrl = "http://demo.pycsw.org/cite/csw?service=CSW&version=2.0.2&request=GetCapabilities";
         File scriptsDir = new File(testProps.getProperty("scriptsDir"));
         File ctlFile = new File(scriptsDir, "ctl"
                 + System.getProperty("file.separator") + "main.xml");
         this.setupOpts.addSource(ctlFile);
-        // run starting test directly to skip input form
+        // run starting test directly to bypass input form
         QName startingTest = new QName(CSW202_NS, "Main", "csw");
         this.runOpts.setTestName(startingTest.toString());
-        this.runOpts.addParam("capabilities.url=" + capabilitiesUrl);
+        for (Object key : sutProps.keySet()) {
+            String paramName = (String) key;
+            StringBuilder kvp = new StringBuilder(paramName);
+            kvp.append("=").append(sutProps.getProperty(paramName));
+            this.runOpts.addParam(kvp.toString());
+        }
         File indexFile = new File(this.sessionDir, "index.xml");
         Index mainIndex = Generator.generateXsl(this.setupOpts);
         mainIndex.persist(indexFile);
